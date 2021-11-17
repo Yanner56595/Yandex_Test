9483dbdb2a0c92c1f4a587a4fac56f3f19680750
Yanner56595 <yanradivilov04@gmail.com>
2021-11-17 20:15:33 +0300

    Second Task

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print('My first git-repo')
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
+from random import randint
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Ui.ui', self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        for i in range(randint(1, 15)):
+            size = randint(50, 500)
+            qp.drawEllipse(randint(0, 1000), randint(0, 700), size, size)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
